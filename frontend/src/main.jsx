99a7e5d61beb5ade0b21d8c94004c2c0b7d78bd2
ΝΑΤΑΛΙΑ ΜΠΑΡΚΑ <146822421+Natalia210305@users.noreply.github.com>
2026-05-18 14:30:42 +0300

    messages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,8 +8,12 @@ import Events from './pages/Events'
 import AdminUsers from './pages/AdminUsers'
 import CreateEvent from './pages/CreateEvent'
 import MyEvents from './pages/MyEvents'
+<<<<<<< HEAD
+import Messages from './pages/Messages'
+=======
 import EventDetail from './pages/EventDetail'
 import Navbar from './components/Navbar'
+>>>>>>> 3183b68517f153da4b7a3ca536061e36e61123d3
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -23,6 +27,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/admin/users" element={<AdminUsers />} />
         <Route path="/create-event" element={<CreateEvent />} />
         <Route path="/my-events" element={<MyEvents />} />
+        <Route path="/messages" element={<Messages />} />
         <Route path="/events/:id" element={<EventDetail />} />
       </Routes>
     </BrowserRouter>
