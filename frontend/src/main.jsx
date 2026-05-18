1888bd334833706b1bbb9ab602abaad23a539fee
ΝΑΤΑΛΙΑ ΜΠΑΡΚΑ <146822421+Natalia210305@users.noreply.github.com>
2026-05-18 14:31:32 +0300

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,12 +8,9 @@ import Events from './pages/Events'
 import AdminUsers from './pages/AdminUsers'
 import CreateEvent from './pages/CreateEvent'
 import MyEvents from './pages/MyEvents'
-<<<<<<< HEAD
 import Messages from './pages/Messages'
-=======
 import EventDetail from './pages/EventDetail'
 import Navbar from './components/Navbar'
->>>>>>> 3183b68517f153da4b7a3ca536061e36e61123d3
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
